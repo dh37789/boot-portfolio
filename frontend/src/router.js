303d6b668c6f53f3fbaa0f37f529c094fe1be547
MyeonghakOh <dhaudgkr@gmail.com>
2019-07-19 13:09:28 +0900

    0.9 update webpack4

---
+++ frontend/src/router.js
@@ -0,0 +1,70 @@
+import Vue from "vue";
+import Router from "vue-router";
+import AppHeader from "./layout/AppHeader";
+import AppFooter from "./layout/AppFooter";
+import Components from "./views/Components.vue";
+import Home from "./views/Home.vue";
+import Landing from "./views/Landing.vue";
+import Login from "./views/Login.vue";
+import Login2 from "./views/Login2.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  linkExactActiveClass: "active",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      components: {
+        header: AppHeader,
+        default: Home,
+        footer: AppFooter
+      }
+    },
+    {
+      path: "/landing",
+      name: "landing",
+      components: {
+        header: AppHeader,
+        default: Landing,
+        footer: AppFooter
+      }
+    },
+    {
+      path: "/components",
+      name: "components",
+      components: {
+        header: AppHeader,
+        default: Components,
+        footer: AppFooter
+      }
+    },
+    {
+      path: "/login",
+      name: "login",
+      components: {
+        header: AppHeader,
+        default: Login,
+        footer: AppFooter
+      }
+    },
+    {
+      path: "/login2",
+      name: "login2",
+      components: {
+        header: AppHeader,
+        default: Login2,
+        footer: AppFooter
+      }
+    }
+  ],
+  scrollBehavior: to => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
+});
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+      <!-- shape Hero -->
+      <section class="section-shaped section-lg my-0">
+        <div class="shape shape-style-2 shape-default shape-skew">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="container pt-lg-md">
+          <div class="row justify-content-center">
+              <div class="col-lg-5">
+                  <card type="secondary" shadow
+                        header-classes="bg-white pb-5"
+                        body-classes="px-lg-5 py-lg-5"
+                        class="border-0">
+                      <template>
+                          <div class="btn-wrapper text-center">
+                          </div>
+                      </template>
+                      <template>
+                          <form role="form">
+                              <base-input alternative
+                                          class="mb-3"
+                                          placeholder="Email"
+                                          addon-left-icon="ni ni-email-83">
+                              </base-input>
+                              <base-input alternative
+                                          type="password"
+                                          placeholder="Password"
+                                          addon-left-icon="ni ni-lock-circle-open">
+                              </base-input>
+                              <base-checkbox>
+                                  Remember me
+                              </base-checkbox>
+                              <div class="text-center mt-3">
+                                 <base-button block type="primary">
+                                   Sign In
+                                 </base-button>
+                              <base-button block type="neutral">
+                                <img slot="icon" src="img/icons/common/google.svg">
+                                  Google Login
+                              </base-button>
+                              </div>
+                          </form>
+                      </template>
+                  </card>
+              </div>
+          </div>
+        </div>
+      </section>
+      <!-- 1st Hero Variation -->
+</template>
