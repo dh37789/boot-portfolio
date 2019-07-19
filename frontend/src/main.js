ab0c95c14a93b06a8abeb2231eae473d25549c98
MyeonghakOh <dhaudgkr@gmail.com>
2019-07-19 15:09:02 +0900

    0.92 eslint issue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import Argon from "./plugins/argon-kit"
-// import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import Argon from "./plugins/argon-kit";
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.use(Argon)
@@ -10,4 +10,3 @@ new Vue({
     router,
     render: h => h(App)
 }).$mount('#app')
-
