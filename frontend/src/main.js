56e97fe9ca8a97ae1d97cde29bc4a37758385405
dhaudgkr <dhaudgkr@gmail.com>
2019-08-02 22:10:55 +0900

    add Vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './router/router';
 import Argon from "./plugins/argon-kit";
+import { store } from './store/store';
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.use(Argon)
 new Vue({
+    store,
     router,
     render: h => h(App)
 }).$mount('#app')
