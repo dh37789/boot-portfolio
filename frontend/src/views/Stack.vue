9a7d268ab4cfb46b36d1a3fb823c506a986596ff
MyeonghakOh <dhaudgkr@gmail.com>
2019-08-06 17:46:51 +0900

    add Skill Icon

--- frontend/src/views/Stack.vue
+++ frontend/src/views/Stack.vue
@@ -16,20 +16,40 @@
           <div class="row my-4">
             <div class="col-12 text-left lang">
                 <img class="skillIcon mx-3" src="img/skill/java.png">
-                <img class="skillIcon p-2 mx-3" src="img/skill/html5.png">
+                <img class="skillIcon mx-3" src="img/skill/html5.png">
                 <img class="skillIcon mx-3" src="img/skill/css3.png">
                 <img class="skillIcon mx-3" src="img/skill/javascript.png">
             </div>
           </div>
         </div>
-        <div class="col-12 text-left">
+        <div class="col-12 text-left lang">
           <h1 class="display2">Framework</h1>
+          <div class="row my-4">
+            <div class="col-12 text-left lang">
+                <img class="skillIcon mx-3" src="img/skill/springframework2.png">
+                <img class="skillIcon mx-3" src="img/skill/boot.png">
+                <img class="skillIcon mx-3" src="img/skill/Vue.png">
+                <img class="skillIcon mx-3" src="img/skill/nodejs.png">
+            </div>
+          </div>
         </div>
         <div class="col-12 text-left">
           <h1 class="display2">Database</h1>
+          <div class="row my-4">
+            <div class="col-12 text-left lang">
+                <img class="skillIcon mx-3" src="img/skill/oracle.png">
+                <img class="skillIcon mx-3" src="img/skill/maria.png">
+            </div>
+          </div>
         </div>
         <div class="col-12 text-left">
           <h1 class="display2">Etc..</h1>
+          <div class="row my-4">
+            <div class="col-12 text-left lang">
+                <img class="skillIcon mx-3" src="img/skill/git.png">
+                <img class="skillIcon mx-3" src="img/skill/svn.png">
+            </div>
+          </div>
         </div>
       </div>  
     </section>
