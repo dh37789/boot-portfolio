02b5bdf237009f35489b38802f31b9fa5a3eb7e1
MyeonghakOh <dhaudgkr@gmail.com>
2019-08-07 17:46:24 +0900

    update stack

--- frontend/src/views/Stack.vue
+++ frontend/src/views/Stack.vue
@@ -12,42 +12,42 @@
     <section class="section">
       <div class="container">
         <div class="col-12 text-left">
-          <h1 class="display2">Language</h1>
-          <div class="row my-4">
+          <h1 class="display2"><Strong>Language</Strong></h1>
+          <div class="row my-2">
             <div class="col-12 text-left lang">
-                <img class="skillIcon mx-3" src="img/skill/java.png">
-                <img class="skillIcon mx-3" src="img/skill/html5.png">
-                <img class="skillIcon mx-3" src="img/skill/css3.png">
-                <img class="skillIcon mx-3" src="img/skill/javascript.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/java.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/html5.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/css3.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/javascript.png">
             </div>
           </div>
         </div>
         <div class="col-12 text-left lang">
-          <h1 class="display2">Framework</h1>
-          <div class="row my-4">
+          <h1 class="display2"><Strong>Framework</Strong></h1>
+          <div class="row my-2">
             <div class="col-12 text-left lang">
-                <img class="skillIcon mx-3" src="img/skill/springframework2.png">
-                <img class="skillIcon mx-3" src="img/skill/boot.png">
-                <img class="skillIcon mx-3" src="img/skill/Vue.png">
-                <img class="skillIcon mx-3" src="img/skill/nodejs.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/springframework2.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/boot.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/Vue.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/nodejs.png">
             </div>
           </div>
         </div>
         <div class="col-12 text-left">
-          <h1 class="display2">Database</h1>
-          <div class="row my-4">
+          <h1 class="display2"><Strong>Database</Strong></h1>
+          <div class="row my-2">
             <div class="col-12 text-left lang">
-                <img class="skillIcon mx-3" src="img/skill/oracle.png">
-                <img class="skillIcon mx-3" src="img/skill/maria.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/oracle.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/maria.png">
             </div>
           </div>
         </div>
         <div class="col-12 text-left">
-          <h1 class="display2">Etc..</h1>
-          <div class="row my-4">
+          <h1 class="display2"><Strong>Version control</Strong></h1>
+          <div class="row my-2">
             <div class="col-12 text-left lang">
-                <img class="skillIcon mx-3" src="img/skill/git.png">
-                <img class="skillIcon mx-3" src="img/skill/svn.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/git.png">
+                <img class="skillIcon mx-3 mb-2" src="img/skill/svn.png">
             </div>
           </div>
         </div>
