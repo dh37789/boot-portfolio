d3559c95960cc83e1d439f27eb2788dfd5a94260
dhaudgkr <dhaudgkr@gmail.com>
2019-08-09 23:13:18 +0900

    clean import

--- src/main/java/com/mho/portfolio/domain/UserSkill.java
+++ src/main/java/com/mho/portfolio/domain/UserSkill.java
@@ -1,15 +1,12 @@
 package com.mho.portfolio.domain;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
