b9df667fa061d3f1d1204101b16dcbb2f7f7a5e9
dhaudgkr <dhaudgkr@gmail.com>
2019-08-10 22:23:25 +0900

    stack end

--- frontend/src/views/Stack.vue
+++ frontend/src/views/Stack.vue
@@ -11,32 +11,79 @@
     </section>
     <section class="section">
       <div class="container">
-        <div v-for="(list, idx) in lists" v-bind:key="idx" class="col-12 text-left">
+        <div class="col-12 text-left">
           <h1 class="display2"><Strong>Language</Strong></h1>
-          <div class="row my-2">
+          <div class="row my-2 ml-2">
             <div class="col-12 text-left lang">
-                <img class="skillIcon mx-3 mb-2" v-bind:src="'img/skill/' + list.skill.skill_img">
+              <template v-for="(list, idx) in lists"  >
+                <div class="wapper" :key="idx" v-if="list.skill.skill_cls_no == '1'">
+                  <img class="skillIcon" v-if="list.skill.skill_cls_no == '1'" v-bind:src="'img/skill/' + list.skill.skill_img">
+                  <div class="links" v-if="list.skill.skill_cls_no == '1'">
+                    <p class="title">{{ list.skill.skill_nm }}</p>
+                    <p v-if="list.skill_lev == '1'">Beginner</p>
+                    <p v-if="list.skill_lev == '2'">Normal</p>
+                    <p v-if="list.skill_lev == '3'">Intermediate</p>
+                    <p v-if="list.skill_lev == '4'">Advanced</p>
+                  </div>
+                </div>
+              </template>
             </div>
           </div>
         </div>
         <div class="col-12 text-left lang">
           <h1 class="display2"><Strong>Framework</Strong></h1>
-          <div class="row my-2">
+          <div class="row my-2 ml-2">
             <div class="col-12 text-left lang">
+              <template v-for="(list, idx) in lists">
+                <div class="wapper" :key="idx"  v-if="list.skill.skill_cls_no == '2'">
+                  <img class="skillIcon" v-if="list.skill.skill_cls_no == '2'" v-bind:src="'img/skill/' + list.skill.skill_img">
+                  <div class="links" v-if="list.skill.skill_cls_no == '2'">
+                    <p class="title">{{ list.skill.skill_nm }}</p>
+                    <p v-if="list.skill_lev == '1'">Beginner</p>
+                    <p v-if="list.skill_lev == '2'">Normal</p>
+                    <p v-if="list.skill_lev == '3'">Intermediate</p>
+                    <p v-if="list.skill_lev == '4'">Advanced</p>
+                  </div>
+                </div>
+              </template>
             </div>
           </div>
         </div>
         <div class="col-12 text-left">
           <h1 class="display2"><Strong>Database</Strong></h1>
-          <div class="row my-2">
+          <div class="row my-2 ml-2">
             <div class="col-12 text-left lang">
+              <template v-for="(list, idx) in lists">
+                <div class="wapper" :key="idx"  v-if="list.skill.skill_cls_no == '3'">
+                  <img class="skillIcon" v-if="list.skill.skill_cls_no == '3'" v-bind:src="'img/skill/' + list.skill.skill_img">
+                  <div class="links" v-if="list.skill.skill_cls_no == '3'">
+                    <p class="title">{{ list.skill.skill_nm }}</p>
+                    <p v-if="list.skill_lev == '1'">Beginner</p>
+                    <p v-if="list.skill_lev == '2'">Normal</p>
+                    <p v-if="list.skill_lev == '3'">Intermediate</p>
+                    <p v-if="list.skill_lev == '4'">Advanced</p>
+                  </div>
+                </div>
+              </template>
             </div>
           </div>
         </div>
         <div class="col-12 text-left">
           <h1 class="display2"><Strong>Version control</Strong></h1>
-          <div class="row my-2">
+          <div class="row my-2 ml-2">
             <div class="col-12 text-left lang">
+              <template v-for="(list, idx) in lists"  >
+                <div class="wapper" :key="idx"  v-if="list.skill.skill_cls_no == '4'">
+                  <img class="skillIcon" v-if="list.skill.skill_cls_no == '4'" v-bind:src="'img/skill/' + list.skill.skill_img">
+                  <div class="links" v-if="list.skill.skill_cls_no == '4'">
+                    <p class="title">{{ list.skill.skill_nm }}</p>
+                    <p v-if="list.skill_lev == '1'">Beginner</p>
+                    <p v-if="list.skill_lev == '2'">Normal</p>
+                    <p v-if="list.skill_lev == '3'">Intermediate</p>
+                    <p v-if="list.skill_lev == '4'">Advanced</p>
+                  </div>
+                </div>
+              </template>
             </div>
           </div>
         </div>
@@ -51,7 +98,8 @@ export default {
     data() {
         return{
             user_id: 'dhaudgkr',
-            lists: []
+            lists: [],
+            language: true
         }
     },
     methods: {
@@ -64,6 +112,9 @@ export default {
             .catch((err) => {
                 console.log(err);
             })
+        },
+        langSw: function() {
+            this.language = false;
         }
     },
     created: function(){
@@ -75,10 +126,11 @@ export default {
 .lang{
   justify-content: flex-start;
 }
+
 .skillIcon{
-  border: 2px solid lightgray;
   border-radius: 30%;
   width: 130px;
   height: 130px;
+  transition: .5s ease;
 }
 </style>
