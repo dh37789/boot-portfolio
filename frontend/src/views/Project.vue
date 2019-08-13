05853256a884da24e6b9419855d3f85b4c0ea1fa
MyeonghakOh <dhaudgkr@gmail.com>
2019-08-13 17:18:05 +0900

    build project

--- frontend/src/views/Project.vue
+++ frontend/src/views/Project.vue
@@ -17,21 +17,45 @@
               <img class="projectImg" v-lazy="'img/brand/window10.png'"/>
             </div>
             <div class="card-body bg-white">
-              <h5 class="card-title"><strong>JavaFX 메모장</strong></h5>
+              <h5 class="card-title">
+                <strong>JavaFX 메모장</strong>
+                <a class="git" target="_blank" rel="noopener" href="https://github.com/dh37789">
+                  <i class="fa fa-github"></i>
+                </a>
+              </h5>
               <p class="card-text">JavaFx를 이용해 간단한 메모장을 만들어 보았습니다.</p>
+              <h6 class="card-subTitle"><small>사용언어</small></h6>
+              <div class="skill">
+                <badge pill type="default" class="mr-1">Java</badge>
+                <badge pill type="default" class="mr-1">JavaFx</badge>
+              </div>
             </div>
             <div class="card-footer">
+              <small class="text-muted">2019.01.08 ~ 2019.01.10</small>
+              <a href="#">자세히</a>
             </div>
           </div>
-          <div class="card border">
-            <div class="card-header">
-            </div>
+          <div class="card border p-1">
             <div class="card-img">
               <img class="projectImg" v-lazy="'img/brand/window10.png'"/>
             </div>
-            <div class="card-body">
+            <div class="card-body bg-white">
+              <h5 class="card-title">
+                <strong>JavaFX 메모장</strong>
+                <a class="git" target="_blank" rel="noopener" href="https://github.com/dh37789">
+                  <i class="fa fa-github"></i>
+                </a>
+              </h5>
+              <p class="card-text">JavaFx를 이용해 간단한 메모장을 만들어 보았습니다.</p>
+              <h6 class="card-subTitle"><small>사용언어</small></h6>
+              <div class="skill">
+                <badge pill type="default" class="mr-1">Java</badge>
+                <badge pill type="default" class="mr-1">JavaFx</badge>
+              </div>
             </div>
             <div class="card-footer">
+              <small class="text-muted">2019.01.08 ~ 2019.01.10</small>
+              <a href="#">자세히</a>
             </div>
           </div>
         </div>
@@ -55,5 +79,11 @@ export default {};
 }
 .card-img{
   border-color: white;
+}
+.card-footer a{
+  float: right;
+}
+.git{
+  float:right;
 }
 </style>
