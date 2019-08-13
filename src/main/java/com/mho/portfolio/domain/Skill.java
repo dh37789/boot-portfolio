d7474fd0d0050f72989fa549578033e5e6920c11
dhaudgkr <dhaudgkr@gmail.com>
2019-08-13 23:49:54 +0900

    migration intelliJ

--- src/main/java/com/mho/portfolio/domain/Skill.java
+++ src/main/java/com/mho/portfolio/domain/Skill.java
@@ -15,33 +15,34 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "SKILL")
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 public class Skill {
-	
-	@Id
-	@Column(name = "SKILL_NO", length = 5)
-	private String skill_no;
-	
-	@Column(name = "SKILL_NM", length = 30)
-	private String skill_nm;
-	
-	@Column(name = "SKILL_CLS_NO", length = 1)
-	private String skill_cls_no;
-	
-	@Column(name = "SKILL_IMG", length = 20)
-	private String skill_img;
 
-	@OneToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "SKILL_CLS_NO",
-			 	insertable = false, updatable = false)
-	private SkillCls skillCls;
-	
-	@Builder
-	public Skill(String skill_no, String skill_nm, String skill_cls_no, String skill_img) {
-		this.skill_no = skill_no;
-		this.skill_nm = skill_nm;
-		this.skill_cls_no = skill_cls_no;
-		this.skill_img = skill_img;
-	}
+  @Id
+  @Column(name = "SKILL_NO", length = 5)
+  private String skill_no;
+
+  @Column(name = "SKILL_NM", length = 30)
+  private String skill_nm;
+
+  @Column(name = "SKILL_CLS_NO", length = 1)
+  private String skill_cls_no;
+
+  @Column(name = "SKILL_IMG", length = 20)
+  private String skill_img;
+
+  @OneToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "SKILL_CLS_NO",
+      insertable = false, updatable = false)
+  private SkillCls skillCls;
+
+  @Builder
+  public Skill(String skill_no, String skill_nm, String skill_cls_no, String skill_img) {
+    this.skill_no = skill_no;
+    this.skill_nm = skill_nm;
+    this.skill_cls_no = skill_cls_no;
+    this.skill_img = skill_img;
+  }
 }
